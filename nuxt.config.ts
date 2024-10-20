28bc49186f684da92a7c235fd580da866638cdc8
Ezz Elden <61851756+ezzeldem@users.noreply.github.com>
2024-10-20 20:17:36 +0300

    ss

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+  },
+});
